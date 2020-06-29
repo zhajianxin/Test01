901b3f3fd599699b87b345800a796e03a49024d5
sinoservices <sinoservices-sh@outlook.com>
2020-06-29 09:25:39 +0800

    修改User.java

--- src/main/java/com/zjx/pojo/User.java
+++ src/main/java/com/zjx/pojo/User.java
@@ -1,9 +1,37 @@
 package com.zjx.pojo;
 
 public class User {
-
+	private int id;
+	
+	private String name;
+	
+	private String email;
+	
 	public User() {
-		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
